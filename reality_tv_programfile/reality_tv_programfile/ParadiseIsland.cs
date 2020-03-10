08d2e2a35ab071d4584635f177497fa8a337a3ba
bearls <61299567+bearls@users.noreply.github.com>
2020-03-10 18:50:09 -0500

    Add files via upload

---
+++ reality_tv_programfile/reality_tv_programfile/ParadiseIsland.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace reality_tv_programfile
+{
+
+	class ParadiseIslandChild : tvshowParent
+    {
+        public string gender;
+        public string nameOfBoyOrGirlFriend;
+        public int yearsDating;
+
+        public ParadiseIslandChild(string gender, string nameOfBoyOrGirlFriend, int yearsDating,
+            string firstName, string lastName, string dateOfBirth, string address, string city, string state, int zip,
+            string phoneNumber, string email) : base(firstName, lastName, dateOfBirth, address, city, state, zip, phoneNumber, email)
+        {
+
+            this.gender = gender;
+            this.nameOfBoyOrGirlFriend = nameOfBoyOrGirlFriend;
+            this.yearsDating = yearsDating;
+        }
+
+        public override void Accept()
+        {
+            base.Accept();
+            Console.WriteLine("You've been accepted to Paradise Island");
+        }
+    }
+}
+
+
---
+++ reality_tv_programfile/reality_tv_programfile/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace reality_tv_programfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HouseHunterChild hhContestant = new HouseHunterChild("Full Stack Web Developer", 100000, 6, 3, 2, "Omar", "Smith", "2/4/1975",
+                "111 Bravo Way", "Los Angeles", "California", 90001, "888-233-1234", "omar.smith@hotmail.com");
+
+            ParadiseIslandChild piContestant = new ParadiseIslandChild("Female", "Carl", 2, "Patti", "Johnson", "5/4/1994", "211 Love Rd", "Los Angeles",
+                "California", 90001, "988-444-1234", "patti.johnson@gmail.com");
+
+            AboveDeckChild adContestant = new AboveDeckChild(20, "American", "Captian", "Lee", "7/12/1964", "311 Bravo Rd", "Los Angeles",
+            "California", 90001, "711-333-1234", "captian@gmail.com");
+
+            hhContestant.Submit();
+            hhContestant.Accept();
+
+            piContestant.Submit();
+            piContestant.Accept();
+
+            adContestant.Submit();
+            adContestant.Accept();
+
+
+        }
+    }
+}
